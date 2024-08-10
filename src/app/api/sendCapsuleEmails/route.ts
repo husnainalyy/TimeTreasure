91b25443f6fbdd462efa14dff6b6fa9161ad220c
Husnain Ali <128280622+husnainalyy@users.noreply.github.com>
2024-08-10 05:31:30 +0500

    Update route.ts

--- src/app/api/sendCapsuleEmails/route.ts
+++ src/app/api/sendCapsuleEmails/route.ts
@@ -6,7 +6,7 @@ import { sendCapsuleOpeningEmail } from '@/helpers/sendCapsuleOpeningEmail';
 
 export async function GET(req: NextRequest) {
         console.log('Cron job triggered');
-        console.log('Cron job has started.');
+        console.log('Cron job has started');
     try {
         await dbConnect();
         console.log("Database connected");
